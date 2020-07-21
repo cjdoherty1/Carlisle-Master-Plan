1b03ea77c539400e5ed54b17f1cebe64aa629fb9
cjdoherty1 <cjdoherty883@gmail.com>
2020-07-20 21:46:38 -0400

    fixed build errors

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,12 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="4"/>
+      <v-col cols="4" />
       <v-col cols="4">
         <v-card>
-          <h1 style="font-size:30pt; text-align:center;" class="py-2"> Contact Us! </h1>
+          <h1 style="font-size:30pt; text-align:center;" class="py-2">
+            Contact Us!
+          </h1>
         </v-card>
       </v-col>
     </v-row>
@@ -12,14 +14,9 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Component from 'vue-class-component'
+import Vue from "vue";
+import Component from "vue-class-component";
 
-@Component({
-
-})
-
-export default class Contact extends Vue{
-  
-}
+@Component({})
+export default class Contact extends Vue {}
 </script>
