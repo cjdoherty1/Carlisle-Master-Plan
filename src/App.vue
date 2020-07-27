f47528ad24804cdcd0f501fa99fcdd286f7e7d40
vzhan00 <zhan.v@husky.neu.edu>
2020-07-27 04:14:15 -0400

    Started redesigning to match given design, need to fix icons on app bar and move pages down to account for app bar change to accomodate transparency

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
   <v-app style="backgroundColor: #D3D3D3">
-    <v-app-bar app color="#1E2011" dark height="85">
-      <div style="padding-bottom: 10px;">
+    <v-app-bar color="transparent" dark height="90" absolute top>
+      <div style="padding-bottom: 10px; padding-top: 10px">
         <v-img
-          :src="require('@/assets/Carlisle_Seal.png')"
-          alt="Carlisle Seal"
+          class="py-2"
+          :src="require('@/assets/Dark Blue_SustainablePath.png')"
+          alt="Carlise Master Plan"
           contain
-          height="80"
+          height="500"
+          width="150"
         />
       </div>
       <v-spacer></v-spacer>
-      <v-btn tile text width="15%" height="85px" to="/">Home</v-btn>
-      <v-btn tile text width="15%" height="85px" to="/participate"
-        >Participate</v-btn
-      >
-      <v-btn tile text width="15%" height="85px" to="/snapshots"
+      <v-btn tile text width="14%" height="85px" to="/">
+        <v-img
+          :src="require('@/assets/home.png')"
+          alt="Home"
+          contain
+          height="50"
+          width="20"
+          absolute
+          top
+          :style="{ left: '7%', transform: 'translateY(0%) translateX(-50%)' }"
+        >
+          <div
+            absolute
+            top
+            :style="{ transform: 'translateY(105%) translateX(70%)' }"
+          >
+            Home
+          </div>
+        </v-img>
+      </v-btn>
+      <v-btn tile text width="17%" height="85px" to="/participate">
+        <v-img
+          :src="require('@/assets/participate.png')"
+          alt="Carlise Master Plan"
+          contain
+          height="50"
+          width="20"
+          absolute
+          top
+          :style="{ left: '6%', transform: 'translateY(0%) translateX(-50%)' }"
+        >
+          <div
+            absolute
+            top
+            :style="{ transform: 'translateY(105%) translateX(70%)' }"
+          >
+            participate
+          </div>
+        </v-img>
+      </v-btn>
+      <v-btn tile text width="17%" height="85px" to="/snapshots"
         >Snapshots</v-btn
       >
-      <v-btn tile text width="15%" height="85px" to="/project_info"
+      <v-btn tile text width="17%" height="85px" to="/project_info"
         >Project Info</v-btn
       >
-      <v-btn tile text width="15%" height="85px" to="/contact">Contact</v-btn>
+      <v-btn tile text width="17%" height="85px" to="/contact">Contact</v-btn>
     </v-app-bar>
 
     <v-content>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <v-img
-      :src="require('@/assets/Great_Brook_Banner.png')"
+      :src="require('@/assets/Carlisle Center Aerial.png')"
       alt="Great Brook Banner"
-      height="300px"
+      height="760px"
     >
+      <h1
+        style="font-size:60pt; text-align:center; margin-top:200px; color: white"
+      >
+        Carlisle Forward Master Plan
+      </h1>
+      <v-btn
+        color="#3949AB"
+        dark
+        x-large="true"
+        absolute
+        top
+        :style="{ left: '50%', transform: 'translateY(850%) translateX(-50%)' }"
+        >Click Here to Help Plan Carlisle's Future!</v-btn
+      >
     </v-img>
     <v-container>
       <v-row>
         <v-col cols="4">
           <v-card height="400px">
-            <v-card-title class="layout justify-center"> Welcome </v-card-title>
+            <v-card-title class="layout justify-center">Welcome</v-card-title>
             <v-card-text>
               Mom's spaghetti, knees weak arms are heavy, there's vomit on the
               sweater alread, palm's are sweaty, he's nervous but on the surface
@@ -23,9 +37,9 @@
         </v-col>
         <v-col cols="4">
           <v-card height="400px">
-            <v-card-title class="layout justify-center">
-              Upcoming Events
-            </v-card-title>
+            <v-card-title class="layout justify-center"
+              >Upcoming Events</v-card-title
+            >
             <v-card-text>
               Mom's spaghetti, knees weak arms are heavy, there's vomit on the
               sweater alread, palm's are sweaty, he's nervous but on the surface
