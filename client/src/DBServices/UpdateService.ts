3870b3492383423d47d6f4db5260a6fb2bdf6cc6
cjdoherty1 <cjdoherty883@gmail.com>
2020-08-11 18:02:09 -0400

    adding heroku support

--- client/src/DBServices/UpdateService.ts
+++ client/src/DBServices/UpdateService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/api/updates/";
+const url = "/api/updates/";
 
 type Update = {
   text: string;
--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,13 @@
+const path = require('path');
+
 module.exports = {
-  transpileDependencies: ["vuetify", "vue-clamp", "resize-detector"]
-};
+  transpileDependencies: ["vuetify", "vue-clamp", "resize-detector"],
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000'
+      }
+    }
+  }
+}
